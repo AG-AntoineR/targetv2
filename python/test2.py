7dc74d0f4c529c5259049eaf8db63cd84d342d6b
AG-AntoineR <antoine.rineau@auguria.fr>
2022-06-27 11:00:07 +0200

    python commit

--- python/test2.py
+++ python/test2.py
@@ -1,1 +1,13 @@
-KEKW
+import git
+
+rw_dir = '/home/antoine/Bureau/targetrepo'
+repo = git.Repo(rw_dir)
+
+def commit(repo):
+    repo.git.add('--all')
+    repo.git.commit('-m', 'python commit')
+    repo.git.push()
+
+commit(repo)
+#git_push = "git add -A ; git commit -m 'Python commit' ; git push --repo  https://AG-AntoineR:Megalovania2000@bitbucket.org/targetv2.git --all "
+#git.push("--repo https://AG-AntoineR:Megalovania2000@bitbucket.org/targetv2.git --all")
